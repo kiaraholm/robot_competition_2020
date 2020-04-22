00d139a1ceb0c2d8f5b2c9969151eaaf41e4671a
Kiara Holm <kholm@vassar.edu>
2020-04-22 11:50:15 -0400

    Complete milestone 1

--- Milestone 1/milestone_1_code/milestone_1_code.ino
+++ Milestone 1/milestone_1_code/milestone_1_code.ino
@@ -1,9 +1,32 @@
+#include <Wire.h>
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  stop_bot();
+  delay(500);
+
+  drive_forward();
+  delay(5000);
+  
+  stop_bot();
+  delay(500);
+
+}
+
+
+void stop_bot() {
+    motors.setLeftSpeed(0);
+    motors.setRightSpeed(0);
+}
+
+void drive_forward() { 
+    motors.setLeftSpeed(200);
+    motors.setRightSpeed(185);  
 }
